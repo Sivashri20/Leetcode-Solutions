93599cce3ba51cdf98738c01f0210a9369793bf3
Sivashri20 <157286820+Sivashri20@users.noreply.github.com>
2024-12-27 21:00:39 +0530

    Create Solution.java

---
+++ Find-the-Difference/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+     int res=0;
+     for(char c:s.toCharArray()) 
+     {
+        res^=c;
+     }  
+     for(char c:t.toCharArray())
+     {
+        res^=c;
+     }
+     return (char)res;
+    }
+}
